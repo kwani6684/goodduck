8821688fe639a7309bab6bfc4d3d3b713a20df32
kwani6684 <jack0910@naver.com>
2023-10-28 01:05:56 +0900

    🎨 Change detail format

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -19,15 +19,19 @@ export default async function Detail(props: PropType) {
   return (
     <div>
       <div className='pt-12 px-12 lg:px-80 justify-items-center md:px-40'>
+        <div className='flex justify-between align-center'>
+        <h1 className='pt-4 pb-4 font-bold leading-7 text-gray-900'>{result.title }</h1>
+          <Link href={`../../edit/${result._id}`}>수정</Link>
+        </div>
         <div className='border-b border-gray-900/10 pb-12'>
-          <h1 className='pt-4 pb-4 font-bold leading-7 text-gray-900'>{result.title }</h1>
-          <p className='mt-1 text-sm leading-6 text-gray-600 border-b border-gray-500 pb-6'>Use a permanent address where you can receive mail.</p>
           
+          <p className='mt-1 text-sm leading-6 text-gray-600 border-b border-gray-500 pb-6'>{result.writer }&apos;s article</p>
+
           <div className=' grid grid-cols-1 gap-x-6 gap-y-4 '>
           <div className='mt-8' dangerouslySetInnerHTML={{ __html: content }} />
           </div>
         </div>
-        <Link href={`../../edit/${result._id}`}>수정</Link>
+        
       <Comment resultId={result._id.toString()} />
       </div>
      

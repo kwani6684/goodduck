c551fa34dfa4ec07a9f979bb8cd6c465999eb4f8
kwani6684 <jack0910@naver.com>
2023-10-28 01:24:05 +0900

    🎨 Improve session approch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
-import "./globals.css";
+import './globals.css';
 
-import { Inter } from "next/font/google";
-import Link from "next/link";
+import { Inter } from 'next/font/google';
+import Link from 'next/link';
 
-import DropMenu from "./components/dropmenu";
-import LoginBtn from "./account/login/LoginBtn";
+import DropMenu from './components/dropmenu';
+import LoginBtn from './account/login/LoginBtn';
 
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import LogoutBtn from "./account/login/LogoutBtn";
-const inter = Inter({ subsets: ["latin"] });
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import LogoutBtn from './account/login/LogoutBtn';
+const inter = Inter({ subsets: ['latin'] });
 
 export interface MenuProps {
   name: string;
@@ -24,10 +24,9 @@ export interface SessionType {
 }
 
 const navigation: MenuProps[] = [
-  { name: "Category", href: "/category" },
-  { name: "Write", href: "/write" },
-  { name: "Lists", href: "/lists" },
-  { name: "Mypage", href: "/mypage" },
+  { name: 'Category', href: '/category' },
+  { name: 'Lists', href: '/lists' },
+  { name: 'Mypage', href: '/mypage' },
 ];
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
@@ -36,37 +35,43 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     console.log(session.user.role);
   }
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <nav className=" flex justify-between p-6 lg:px-8" aria-label="Global">
-          <div className="flex lg:flex-1">
-            <Link href="/" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+        <nav className=' flex justify-between p-6 lg:px-8' aria-label='Global'>
+          <div className='flex lg:flex-1'>
+            <Link href='/' className='-m-1.5 p-1.5'>
+              <span className='sr-only'>Your Company</span>
+              <img className='h-8 w-auto' src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600' alt='' />
             </Link>
           </div>
 
-          <div className=" flex lg:hidden">
-            <span className="mr-4 font-semibold">{session?.user.name}</span>
+          <div className=' flex lg:hidden'>
+            <span className='mr-4 font-semibold'>{session?.user.name}</span>
             <DropMenu menu={navigation} session={session} />
           </div>
-          <div className="hidden lg:flex lg:gap-x-12">
+          <div className='hidden lg:flex lg:gap-x-12'>
             {navigation.map((item) => (
-              <Link key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+              <Link key={item.name} href={item.href} className='text-sm font-semibold leading-6 text-gray-900'>
                 {item.name}
               </Link>
             ))}
+            {session?(<Link href={'/write'}  className='text-sm font-semibold leading-6 text-gray-900'>write</Link>):('')}
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <div className='hidden lg:flex lg:flex-1 lg:justify-end'>
             {session ? (
               <span>
-                <span className="font-semibold mr-4">{session.user.name}</span>
+                <span className='font-semibold mr-4'>{session.user.name}</span>
                 {/* <span className="font-semibold mr-4">{session.user?.name}</span> */}
 
                 <LogoutBtn />
               </span>
             ) : (
-              <LoginBtn />
+              <span>
+                <Link href='/register' className='font-semibold mr-4'>
+                  Register
+                </Link>
+                <LoginBtn />
+              </span>
             )}
           </div>
         </nav>

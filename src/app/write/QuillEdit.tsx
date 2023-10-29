4dae76e6bf60624a2a9fd93ce4dab99fbca77630
kwani6684 <jack0910@naver.com>
2023-10-29 23:42:20 +0900

    💥 add upload image
    with using asw s3

--- src/app/write/QuillEdit.tsx
+++ src/app/write/QuillEdit.tsx
@@ -1,9 +1,12 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import dynamic from 'next/dynamic';
-import 'react-quill/dist/quill.snow.css'; 
+import 'react-quill/dist/quill.snow.css';
 import { PostType } from '../lists/page';
 import Link from 'next/link';
+import aws from 'aws-sdk';
+import ReactQuill from 'react-quill';
+import Quill from 'react-quill';
 
 export interface CategoryType {
   _id: string;
@@ -13,27 +16,90 @@ export interface CategoryType {
 const QuillEditor = dynamic(() => import('react-quill'), { ssr: false });
 
 export default function Editor({ category }: any) {
+  const quillRef = useRef(null);
   let [content, setContent] = useState('');
   let [title, setTitle] = useState('');
   let [thisCategory, setThisCategory] = useState('스포츠⚽️');
   let [data, setData] = useState<PostType[]>([]);
-
-  
+  useEffect(() => {});
+  const imageHandler = async () => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+    input.addEventListener('change', async () => {
+      //이미지를 담아 전송할 file을 만든다
+      const file = input.files?.[0];
+      try {
+        //업로드할 파일의 이름으로 Date 사용
+        const name = Date.now();
+        //생성한 s3 관련 설정들
+        aws.config.update({
+          accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY,
+          secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_KEY,
+          region: process.env.NEXT_PUBLIC_AWS_REGION,
+          signatureVersion: 'v4',
+        });
+        //앞서 생성한 file을 담아 s3에 업로드하는 객체를 만든다
+        const upload = new aws.S3.ManagedUpload({
+          params: {
+            Bucket: 'goodduckbucket', //버킷 이름
+            Key: `upload/${name}`,
+            Body: file,
+          },
+        });
+        //이미지 업로드 후
+        //곧바로 업로드 된 이미지 url을 가져오기
+        const IMG_URL = await upload.promise().then((res) => res.Location);
+        //useRef를 사용해 에디터에 접근한 후
+        //에디터의 현재 커서 위치에 이미지 삽입
+        const cur = quillRef.current as Quill | null;
+        if (cur) {
+          const editor = cur.getEditor();
+          const range = editor.getSelection();
+          if (range) {
+            // 가져온 위치에 이미지를 삽입한다
+            editor.insertEmbed(range.index, 'image', IMG_URL);
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
   useEffect(() => {
     console.log(thisCategory);
-},[thisCategory])
-  const quillModules = {
-    toolbar: [
-      [{ header: [1, 2, 3, false] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [{ list: 'ordered' }, { list: 'bullet' }],
-      ['link', 'image'],
-      [{ align: [] }],
-      [{ color: [] }],
-      ['code-block'],
-      ['clean'],
-    ],
-  };
+  }, [thisCategory]);
+  // const quillModules = useMemo(() => {
+  //   return {
+  //     toolbar: {
+  //       container:[
+  //       [{ header: [1, 2, 3, false] }],
+  //       ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+  //       [{ list: 'ordered' }, { list: 'bullet' }],
+  //
+  //       ['image'],
+  //       [{ align: [] }],
+  //       [{ color: [] }],
+  //       ['code-block'],
+  //       ['clean'],
+  //       ]
+  //     },
+  //     handlers: {
+  //       image: imageHandler,
+  //     },
+  //   };
+  // }, []);
+  const quillModules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [[{ header: [1, 2, 3, 4, 5, false] }], ['bold', 'underline'], [{ color: [] }], ['link'], ['image']],
+        handlers: {
+          image: imageHandler,
+        },
+      },
+    };
+  }, []);
 
   const quillFormats = [
     'header',
@@ -55,7 +121,6 @@ export default function Editor({ category }: any) {
     setContent(newContent);
     console.log(content);
   };
-  
 
   return (
     <div>
@@ -70,8 +135,7 @@ export default function Editor({ category }: any) {
               <input
                 onChange={(e) => {
                   setTitle(e.currentTarget.value);
-                  console.log(title)
-                  
+                  console.log(title);
                 }}
                 type='text'
                 name='title'
@@ -92,9 +156,7 @@ export default function Editor({ category }: any) {
 
               <select
                 onChange={(e) => {
-                  
                   setThisCategory(e.currentTarget.value);
-
                 }}
                 name='category'
                 id='category'
@@ -120,7 +182,8 @@ export default function Editor({ category }: any) {
             <div className=' block w-full flex-col rounded-md text-gray-900 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'>
               <div className='flex flex-col items-center'>
                 <div className='h-[400px] w-full'>
-                  <QuillEditor
+                  <ReactQuill
+                    ref={quillRef}
                     value={content}
                     onChange={handleEditorChange}
                     modules={quillModules}
@@ -136,20 +199,20 @@ export default function Editor({ category }: any) {
       </div>
 
       <div className='mb-10 flex items-center justify-center gap-x-6'>
-        <button
-          
-          type='button' className='text-lg font-semibold leading-6 text-gray-900'>
+        <button type='button' className='text-lg font-semibold leading-6 text-gray-900'>
           Cancel
         </button>
         <Link
           href='/lists'
           onClick={() => {
-            fetch(`/api/postContent`, { method: "POST", body: JSON.stringify({ title: title, category: thisCategory,content:content }) }).then((response) => {
-              if (response.ok) {
-                console.log("yaho")
-             // '/lists' 로 페이지 이동하는 코드
+            fetch(`/api/postContent`, { method: 'POST', body: JSON.stringify({ title: title, category: thisCategory, content: content }) }).then(
+              (response) => {
+                if (response.ok) {
+                  console.log('yaho');
+                  // '/lists' 로 페이지 이동하는 코드
+                }
               }
-            });
+            );
           }}
           type='submit'
           className='rounded-md bg-indigo-600 px-3 py-2 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'

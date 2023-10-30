e642d7249b8789ddac460d05ce6564dc260d2e22
kwani6684 <jack0910@naver.com>
2023-10-30 20:16:59 +0900

    🛂 update auth
    add category -> only admin
    write post -> only member

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,10 +1,22 @@
-import AddCategory from "./AddCategory";
+import { getServerSession } from 'next-auth';
+import { SessionType } from '../layout';
+import AddCategory from './AddCategory';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
-export default function Category() {
-    return (
+export default async function Category() {
+  let session: SessionType | null = await getServerSession(authOptions);
+
+  return (
+    <div>
+      {session?.user.role === 'admin' ? (
+        <AddCategory />
+      ) : (
         <div>
-            <AddCategory/>
+          <div>권한이 없어요 ㅜ</div>
+          <div>내 게시글이 5개가 되면 카테고리를 추가 할 수 있어요</div>
         </div>
-    )
-    //category 추가 세션 검사
+      )}
+    </div>
+  );
+  //category 추가 세션 검사
 }
--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,5 +1,8 @@
 import { connectDB } from '@/util/database';
 import Editor from './QuillEdit';
+import { SessionType } from '../layout';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
 // import dynamic from 'next/dynamic';
 export interface CategoryType {
   _id: string;
@@ -9,24 +12,25 @@ export interface CategoryType {
 // var delta = editor.getContents();
 
 export default async function QuillEditor() {
+  let session: SessionType | null = await getServerSession(authOptions);
   const client = (await connectDB) as any;
   const db = client.db('goodduck');
-  let category:CategoryType[] = await db.collection('category').find().toArray();
+  let category: CategoryType[] = await db.collection('category').find().toArray();
   // console.log(delta)
   return (
     <div>
-      
+      {session === null ? (
+        <div>권한 x</div>
+      ) : (
         <div className='pt-12 px-12 lg:px-80 md:px-30'>
           <div className='border-b border-gray-900/10 pb-12'>
             <h4 className='text-base pt-4 font-semibold leading-7 text-gray-900'>QuillEditor Article</h4>
             <p className='mt-1 text-sm leading-6 text-gray-600'>Use a permanent address where you can receive mail.</p>
-            
-              <Editor category={category}></Editor>
-            
+
+            <Editor category={category}></Editor>
           </div>
         </div>
-        
-      
+      )}
     </div>
   );
 }

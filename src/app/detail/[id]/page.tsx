b3a99bd2a1ddc58b09454ac534a2ccc05e8e6536
kwani6684 <jack0910@naver.com>
2023-10-30 09:20:51 +0900

    🔐 update dompurify
    dompurify -> isomorphic-dompurify

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -2,7 +2,8 @@ import { connectDB } from '@/util/database';
 import { ObjectId } from 'mongodb';
 import Link from 'next/link';
 import Comment from './Comment';
-import DOMPurify from 'dompurify';
+
+import dompurify from 'isomorphic-dompurify';
 export interface PropType {
   params: {
     id: string;
@@ -14,7 +15,7 @@ export default async function Detail(props: PropType) {
   const db = client.db('goodduck');
   let result = await db.collection('post').findOne({ _id: new ObjectId(props.params.id) });
   let content = result.content;
-
+  const sanitizer = dompurify.sanitize;
   console.log(props);
   return (
     <div>
@@ -28,7 +29,7 @@ export default async function Detail(props: PropType) {
           <p className='mt-1 text-sm leading-6 text-gray-600 border-b border-gray-500 pb-6'>{result.writer }&apos;s article</p>
 
           <div className=' grid grid-cols-1 gap-x-6 gap-y-4 '>
-          <div className='mt-8' dangerouslySetInnerHTML={{ __html:DOMPurify.sanitize( content) }} />
+          <div className='mt-8' dangerouslySetInnerHTML={{ __html:sanitizer(content) }} />
           </div>
         </div>
         

8ae6740c2937ae83f1f197f97eabc3187461780c
kwani6684 <jack0910@naver.com>
2023-10-30 20:16:04 +0900

    🗃️ add not allow same category

--- src/pages/api/category/postCategory.ts
+++ src/pages/api/category/postCategory.ts
@@ -6,10 +6,16 @@ export default async function Handler(request: NextApiRequest, response: NextApi
   const db = client.db("goodduck");
 
   request.body = JSON.parse(request.body);
+  let result = await db.collection("category").find().toArray();
 
   if (request.method == "POST") {
     if (request.body.category == "") {
       return response.status(500).json("내용써라");
+    }
+    let res = result.find((category: any) => request.body.category === category.value);
+    console.log(res)
+    if (res!==null) {
+      return response.status(500).json("중복임");
     }
     const data = {
       value: request.body.category,

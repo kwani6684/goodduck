3500c7175728f1a79559ec951c9b000f69d1ab42
kwani6684 <jack0910@naver.com>
2023-11-02 01:50:06 +0900

    🐛 fix duplicate category
    null -> undefined

--- src/pages/api/category/postCategory.ts
+++ src/pages/api/category/postCategory.ts
@@ -1,26 +1,26 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { connectDB } from "@/util/database";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { connectDB } from '@/util/database';
 
 export default async function Handler(request: NextApiRequest, response: NextApiResponse) {
   const client = (await connectDB) as any;
-  const db = client.db("goodduck");
+  const db = client.db('goodduck');
 
   request.body = JSON.parse(request.body);
-  let result = await db.collection("category").find().toArray();
+  let result = await db.collection('category').find().toArray();
 
-  if (request.method == "POST") {
-    if (request.body.category == "") {
-      return response.status(500).json("내용써라");
+  if (request.method == 'POST') {
+    if (request.body.category == '') {
+      return response.status(500).json('내용써라');
     }
     let res = result.find((category: any) => request.body.category === category.value);
-    console.log(res)
-    if (res!==null) {
-      return response.status(500).json("중복임");
+    console.log(res);
+    if (res !== undefined) {
+      return response.status(500).json('중복임');
     }
     const data = {
       value: request.body.category,
     };
-    await db.collection("category").insertOne(data);
-    response.status(202).send("add category");
+    await db.collection('category').insertOne(data);
+    response.status(202).send('add category');
   }
 }

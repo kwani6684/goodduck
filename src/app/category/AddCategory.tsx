b5ff164dcc75845c33cfe8de1f6e0fbd79d29388
kwani6684 <jack0910@naver.com>
2023-11-11 23:46:11 +0900

    ✨feat: add addCategory page and update creteria

--- src/app/category/AddCategory.tsx
+++ src/app/category/AddCategory.tsx
@@ -18,49 +18,44 @@ export default function AddCategory() {
   };
   return (
     <div>
-      <div className='py-12 px-12 lg:px-80 md:px-30'>
-        <div className='border-b border-gray-900/10 pb-12'>
-          <h4 className='text-base pt-4 font-semibold leading-7 text-gray-900'>카테고리를 추가해주세염</h4>
-          <p className='mt-1 text-sm leading-6 text-gray-600'>tip. 글쓰기 원하는 카테고리 + 이모지 형태로 추가해주세요 ex) 스포츠⚽️ </p>
-          <input
-            id='input'
-            onChange={(e) => {
-              setCategory(e.currentTarget.value);
-              console.log(data);
-            }}
-          />
-          <button
-            className='border-2 border-yellow-800 text-yellow-800 text-sm uppercase tracking-widest py-2 px-4 mx-2 hover:bg-yellow-800 hover:text-white'
-            onClick={() => {
-              fetch(`/api/category/postCategory`, { method: 'POST', body: JSON.stringify({ category: category }) }).then((response) => {
-                if (response.ok) {
-                  // fetch("/api/comment/getComment")
-                  // .then((r) => r.json())
-                  // .then((result) => setData(result));
-                  // 댓글 바로 보여주는 코드 필요
-                  refreshCategory();
-                }
-                else {
-                  alert('중복임');
-                }
-              });
-            }}
-          >
-            등록
-          </button>
-          {data.length > 0 ? (
-            data.map((item: CategoryType, i: number) => {
-              return (
-                <div key={i}>
-                  <span className='mr-4'>{item.value}</span>
-                </div>
-              );
-            })
-          ) : (
-            <div></div>
-          )}
-        </div>
-      </div>
+      <h4 className='text-base pt-4 font-semibold leading-7 text-gray-900'>카테고리를 추가해주세염</h4>
+      <p className='mt-1 text-sm leading-6 text-gray-600'>tip. 글쓰기 원하는 카테고리 + 이모지 형태로 추가해주세요 ex) 스포츠⚽️ </p>
+      <input
+        id='input'
+        onChange={(e) => {
+          setCategory(e.currentTarget.value);
+          console.log(data);
+        }}
+      />
+      <button
+        className='border-2 border-yellow-800 text-yellow-800 text-sm uppercase tracking-widest py-2 px-4 mx-2 hover:bg-yellow-800 hover:text-white'
+        onClick={() => {
+          fetch(`/api/category/postCategory`, { method: 'POST', body: JSON.stringify({ category: category }) }).then((response) => {
+            if (response.ok) {
+              // fetch("/api/comment/getComment")
+              // .then((r) => r.json())
+              // .then((result) => setData(result));
+              // 댓글 바로 보여주는 코드 필요
+              refreshCategory();
+            } else {
+              alert('중복임');
+            }
+          });
+        }}
+      >
+        등록
+      </button>
+      {data.length > 0 ? (
+        data.map((item: CategoryType, i: number) => {
+          return (
+            <div key={i}>
+              <span className='mr-4'>{item.value}</span>
+            </div>
+          );
+        })
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -3,28 +3,54 @@ import { SessionType } from '../layout';
 import AddCategory from './AddCategory';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { connectDB } from '@/util/database';
+import Link from 'next/link';
 
 export default async function Category() {
   let session: SessionType | null = await getServerSession(authOptions);
   const client = (await connectDB) as any;
   const db = client.db('goodduck');
   let postCount = await db.collection('post').find({ email: session?.user.email }).toArray();
+  let commentCount = await db.collection('comment').find({ email: session?.user.email }).toArray();
   console.log(postCount.length);
-  if (session?.user.role === 'normal' && postCount.length > 2) {
-    await db.collection('userinfo').updateOne({ email: session?.user.email }, { $set: { role: 'squriell' } });
+  if (session?.user.role === 'normal') {
+    if (commentCount.length>4 && postCount.length > 2) {
+      await db.collection('userinfo').updateOne({ email: session?.user.email }, { $set: { role: 'squriell' } });
+    }
   }
+  
 
   return (
-    <div>
-      {session?.user.role === 'admin' || session?.user.role === 'squriell' ? (
-        <AddCategory />
-      ) : (
-        <div>
-          <div>권한이 없어요 ㅜ</div>
-            <div>내 게시글이 5개가 되면 카테고리를 추가 할 수 있어요</div>
-            <div>❗️내 게시글이 5개가 넘었다면 로그아웃 후 재 로그인 하면 카테고리를 추가 할 수 있어요🐿️</div>
-        </div>
-      )}
+    <div className='py-12 px-12 lg:px-80 md:px-30'>
+      <div className='border-b border-gray-900/10 pb-12'>
+        {session?.user.role === 'admin' || session?.user.role === 'squriell' ? (
+          <AddCategory />
+        ) : (
+          <div>
+              <div>❗️내 게시글이 3개가 넘고 댓글을 5개 이상 달았다면 로그아웃 후 재 로그인 하면 카테고리를 추가 할 수 있어요🐿️</div>
+              <div>현재 게시글 수 : {postCount.length} , 현재 댓글 수 :{ commentCount.length}</div>
+              <div className='flex justify-center pt-4'>
+            <Link
+              href='/'
+              className='inline-block border-2 border-yellow-800 font-light text-yellow-800 text-sm uppercase tracking-widest py-3 px-4 mx-2 hover:bg-yellow-800 hover:text-white'
+            >
+              카테고리 보러가기
+                </Link>
+                <Link
+              href='/write'
+              className='inline-block border-2 border-yellow-800 font-light text-yellow-800 text-sm uppercase tracking-widest py-3 px-4 mx-2 hover:bg-yellow-800 hover:text-white'
+            >
+              게시글 쓰러가기
+                </Link>
+                <Link
+              href='/lists'
+              className='inline-block border-2 border-yellow-800 font-light text-yellow-800 text-sm uppercase tracking-widest py-3 px-4 mx-2 hover:bg-yellow-800 hover:text-white'
+            >
+              댓글 달러가기
+                </Link>
+                </div>
+          </div>
+        )}
+      </div>
     </div>
   );
   //category 추가 세션 검사
